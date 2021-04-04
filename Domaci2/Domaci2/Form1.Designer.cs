7dec716a6bf7dd150f2776e3b20f22591f14587c
Ajlag <40441745+Ajlag@users.noreply.github.com>
2021-04-04 21:44:50 +0200

    Add files via upload

---
+++ Domaci2/Domaci2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Domaci2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Crtaj = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Crtaj
+            // 
+            this.Crtaj.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Crtaj.Location = new System.Drawing.Point(194, 618);
+            this.Crtaj.Name = "Crtaj";
+            this.Crtaj.Size = new System.Drawing.Size(181, 82);
+            this.Crtaj.TabIndex = 0;
+            this.Crtaj.Text = "Crtaj";
+            this.Crtaj.UseVisualStyleBackColor = true;
+            this.Crtaj.Click += new System.EventHandler(this.Crtaj_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImage = global::Domaci2.Properties.Resources.v1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.MaximumSize = new System.Drawing.Size(582, 800);
+            this.panel1.MinimumSize = new System.Drawing.Size(582, 600);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 600);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 728);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Crtaj);
+            this.MaximumSize = new System.Drawing.Size(625, 775);
+            this.MinimumSize = new System.Drawing.Size(624, 775);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Crtaj;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Domaci2/Domaci2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Domaci2
+{
+    public partial class Form1 : Form
+    {
+        Boolean glavaDodata = false;
+        Boolean stomakDodaj = false;
+        Boolean rukeDodaj = false;
+        Boolean kolenaDodaj = false;
+        Boolean noga1 = false;
+        Boolean noga2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 500;
+            this.Height = 500;
+
+        }
+
+        private void Crtaj_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            if (glavaDodata == false)
+            {
+
+                Bitmap bitmap = new Bitmap(Properties.Resources.gargamel);
+                g.DrawImage(bitmap, 70, 180, 150, 80);
+                glavaDodata = true;
+
+            }
+            else
+            {
+                if (stomakDodaj == false)
+                {
+                    Bitmap bitmap1 = new Bitmap(Properties.Resources.Stomak);
+                    g.DrawImage(bitmap1, 145, 260, 100, 80);
+                    stomakDodaj = true;
+                }
+                else
+                {
+                    if (rukeDodaj == false)
+                    {
+                        Bitmap bitmap2 = new Bitmap(Properties.Resources.ruke);
+                        g.DrawImage(bitmap2, 65, 260, 90, 80);
+                        rukeDodaj = true;
+                    }
+                    else
+                    {
+                        if (kolenaDodaj == false)
+                        {
+                            Bitmap bitmap3 = new Bitmap(Properties.Resources.Kolena);
+                            g.DrawImage(bitmap3, 92, 340, 175, 80);
+                            kolenaDodaj = true;
+                        }
+                        else
+                        {
+                            if (noga1 == false)
+                            {
+
+                                Bitmap bitmap4 = new Bitmap(Properties.Resources.noga1);
+                                g.DrawImage(bitmap4, 160, 420, 100, 80);
+                                noga1 = true;
+                            }
+                            else
+                            {
+                                if (noga2 == false)
+                                {
+                                    Bitmap bitmap5 = new Bitmap(Properties.Resources.noga2);
+                                    g.DrawImage(bitmap5, 75, 418, 90, 70);
+                                    noga2 = true;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Obesili ste Gargamela");
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+        }
+    }
+}
